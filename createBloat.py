3513a2c8780f7403e7b9db9baf2ab623ce635be6
coolhatena <ingsc.geffroy@gmail.com>
2023-01-12 11:16:43 -0800

    First commit

---
+++ createBloat.py
@@ -0,0 +1,6 @@
+STARTNUMBER = 0
+ENDNUMBER = 300
+
+for i in range(STARTNUMBER, ENDNUMBER):
+	open(f'./FILENAME.jpg', 'a').close()
+	# open(f'./images/19-11-2022-15-3-36-L2-E{i}-fail-new-Rojo-15-0-Cafe-15-0-Azul-15-0-NA-0-0.jpg', 'a').close()
